a1ae6b96f7c7486b3304d0193e4dd7c8c27150c1
Kingcash16 <77807256+Kingcash16@users.noreply.github.com>
2021-02-10 03:01:51 -0600

    Add files via upload
    Homework 1 Zylab 1.20

---
+++ num.py
@@ -0,0 +1,8 @@
+#Joshua Jaja ID:1543343 Zylab 1.20
+userNum = int(input('Enter integer: '))
+print('You entered:', userNum)
+print(userNum, "squared is",userNum*userNum)
+print("And", userNum, "cubed is",userNum*userNum*userNum)
+userNum1 = int(input("Enter another integer:"))
+print(userNum, "+", userNum1, "is",userNum+userNum1)
+print(userNum, "*", userNum1, "is", userNum*userNum1)
