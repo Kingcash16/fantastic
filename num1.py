e04da96da8394ec77097f5e43b19ca43bc9bea27
Kingcash16 <77807256+Kingcash16@users.noreply.github.com>
2021-02-10 11:51:23 -0600

    Homework 1

---
+++ num1.py
@@ -0,0 +1,8 @@
+#Joshua Jaja ID:1543343 Zylab 1.20
+uN = int(input('Enter integer: '))
+print('You entered:', uN)
+print(uN, "squared is", uN*uN)
+print("And", uN, "cubed is", uN*uN*uN)
+uN1 = int(input("Enter another integer:"))
+print(uN, "+", uN1, "is", uN+uN1)
+print(uN, "*", uN1, "is", uN*uN1)
